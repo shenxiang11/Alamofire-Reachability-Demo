827c1290fe7f55833d2140f6d98f3fb60d2f58a0
香饽饽zizizi <863461783@qq.com>
2020-12-30 16:50:33 +0800

    Initial Commit

---
+++ Shared/Alamofire_Reachability_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Alamofire_Reachability_DemoApp.swift
+//  Shared
+//
+//  Created by 沈翔 on 2020/12/30.
+//
+
+import SwiftUI
+
+@main
+struct Alamofire_Reachability_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
